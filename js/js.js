54d83c62c3c5eb31ecd1c8ba20b43a78358bc42e
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2021-11-29 23:04:53 +0200

    Pradzia JS

---
+++ js/js.js
@@ -0,0 +1,10 @@
+let a = 1;
+let tekstas = 'Kintamojo a reiksme: ';
+console.log(tekstas + a);
+
+a = 2;
+console.log(tekstas + a);
+
+let b = 'Jūsų vardas: ';
+let c = 'Vardenis Pavardenis';
+ console.log(b + c);

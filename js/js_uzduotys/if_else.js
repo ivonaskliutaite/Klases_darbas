2903f7d2ef3708b51fe18e6a07f3f3f21eaa4444
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2021-12-05 14:40:03 +0200

    Viskas up to date

---
+++ js/js_uzduotys/if_else.js
@@ -0,0 +1,6 @@
+let x = [a < b];
+let y = [c < d];
+
+if (x = y){
+    console.log()
+}
---
+++ js/js_uzduotys/js_1_1.js
@@ -0,0 +1,27 @@
+let a = 1;
+let tekstas = 'Kintamojo a reiksme: ';
+console.log(tekstas + a);
+
+a = 2;
+console.log(tekstas + a);
+
+let b = 'Jusu vardas: ';
+let c = 'Vardas Pavardenis';
+console.log(b + c);
+
+let metai = 1999;
+let menuo = 12;
+let diena = 28;
+console.log(metai + menuo + diena);
+
+metai = metai %= 1990;
+console.log(metai);
+
+menuo = menuo %= 10;
+console.log(menuo);
+
+diena = diena %= 20;
+console.log(diena);
+
+console.log(metai + menuo + diena);
+
